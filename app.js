3bece66eedbce9541ae95575cce93606e0308db3
Jenesh <jeneshrambo18@gmail.com>
2025-01-30 22:25:38 +0530

    setup html file and ts file

---
+++ app.js
@@ -0,0 +1,4 @@
+function testFunction(num1, num2) {
+    console.log(num1 + num2);
+}
+testFunction(1, 2);
---
+++ app.ts
@@ -0,0 +1,6 @@
+ function testFunction(num1:number, num2:number){
+  console.log(num1+num2)
+}
+
+
+testFunction(1,2);
